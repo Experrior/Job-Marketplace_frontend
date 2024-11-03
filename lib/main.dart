17077a96c9e6119b52847731d590f7fc0b27fe3e
Sergiy Vergun <sergiyvergun01@gmail.com>
2024-11-03 16:40:41 +0100

    Fix macos UI appearance

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 import 'core/app.dart';
 
-void main(){
+Future<void> main() async{
+  await configureMacosWindowUtils();
   runApp(const ProviderScope(child: JobSearchApp()));
+}
+
+Future<void> configureMacosWindowUtils() async {
+
+  const config = MacosWindowUtilsConfig();
+  await config.apply();
 }
