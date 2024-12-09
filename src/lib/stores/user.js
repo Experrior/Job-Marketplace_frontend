c03eb8bd19d1bd0a7848b3352526b5db218cef85
Mateusz Molenda <devexperrior@gmail.com>
2024-12-09 14:37:55 +0100

    fixing ssr deauthentication

--- src/lib/stores/user.js
+++ src/lib/stores/user.js
@@ -1,5 +1,4 @@
-// src/stores/user.js
-import { writable, get } from 'svelte/store';
+import { writable, get, readable } from 'svelte/store';
 import { browser } from '$app/environment';
 
 const template = {
@@ -15,60 +14,45 @@ const template = {
   role: ''
 };
 
-// Initialize the writable store with the template
 const user = writable(template);
 
-// Only execute the following block in the browser
 if (browser) {
-  // Attempt to retrieve the user from localStorage
+
   const storedUser = localStorage.getItem('user');
   if (storedUser) {
     try {
       user.set(JSON.parse(storedUser));
     } catch (e) {
       console.error('Failed to parse user from localStorage:', e);
-      // Optionally reset to template if parsing fails
-      user.set(template);
-      localStorage.removeItem('user');
     }
   }
 
-  // Subscribe to store changes and update localStorage accordingly
   user.subscribe((value) => {
-    // Avoid storing empty template to localStorage
-    if (value && value.jwt) {
-      localStorage.setItem('user', JSON.stringify(value));
-    } else {
-      localStorage.removeItem('user');
-    }
+    localStorage.setItem('user', JSON.stringify(value));
   });
 }
 
-// Function to reset the user store to its default template
 const resetUser = () => user.set(template);
 
-/**
- * Verifies if the user is authenticated based on the presence of a JWT.
- * 
- * @returns {boolean} - Returns `true` if the user is authenticated, `false` otherwise.
- */
+
+
 function verifyUser() {
-  if (!browser) {
-    // During SSR, assume the user is not authenticated
+  if (!browser) return true;
+
+  const currentUser = get(user);
+  if (!currentUser) {
     return false;
   }
 
-  const currentUser = get(user);
+  const userExists = currentUser.jwt;
 
-  if (!currentUser || !currentUser.jwt) {
-    // If no user or JWT is found, reset the store and clear localStorage
+  if (!userExists) {
     resetUser();
     localStorage.removeItem('user');
     console.warn('User is not logged in. Resetting user store and clearing localStorage.');
     return false;
   }
 
-  // User is authenticated
   return true;
 }
 
